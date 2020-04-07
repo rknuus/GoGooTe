4af827fdbfad18e2440445dbe8b1716c951fec64
Raphael Knaus <rknuus@gmail.com>
2020-04-07 17:37:39 +0200

    Turn application into standard tooling application

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,27 @@
-int main() {
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+
+
+using namespace clang::tooling;
+using namespace llvm;
+
+
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static llvm::cl::OptionCategory GoGooTeCategory("GoGooTe options");
+
+// CommonOptionsParser declares HelpMessage with a description of the common
+// command-line options related to the compilation database and input files.
+// It's nice to have this help message in all tools.
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+// A help message for this specific tool can be added afterwards.
+static cl::extrahelp MoreHelp("\n%%TODO(KNR)%%...\n");
+
+int main(int argc, const char **argv) {
+  CommonOptionsParser OptionsParser(argc, argv, GoGooTeCategory);
+  ClangTool Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+  return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
 }
