9130d63d0adf22b66417a7047ec41d33596e90d5
Raphael Knaus <rknuus@gmail.com>
2024-09-08 15:38:23 +0200

    WIP Refactor CMake functions

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,100 +1,135 @@
-function(resolve_absolute_path var base_dir)
-  if (NOT IS_ABSOLUTE ${${var}})
-    get_filename_component(${var}
-      "${${var}}" REALPATH BASE_DIR "${base_dir}")
-    set(${var} "${${var}}" PARENT_SCOPE)
-  endif()
-endfunction()
-
-function(find_llvm_cmake_dir llvm_cmake_directory_out_var)
-  if (DEFINED PATH_TO_LLVM)
-    set(search_paths ${PATH_TO_LLVM})
-  else()
-    if (APPLE)
-      if (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
-        # ARM-based Macs (M1/M2) using Homebrew
-        set(search_paths /opt/homebrew/opt/llvm/lib/cmake/llvm/)
-      else()
-        # Intel-based Macs using Homebrew
-        set(search_paths /usr/local/opt/llvm/lib/cmake/llvm/)
-      endif()
-    elseif (UNIX)
-      # Linux platforms
-      # FIXME(RAKN): generalize to match llvm-18 til future versions
-      set(search_paths
-        /usr/lib/llvm-10/lib/cmake/llvm/      # Example for LLVM 10 on Ubuntu/Debian
-        /usr/lib/llvm-12/lib/cmake/llvm/      # Example for LLVM 12 on Fedora/RHEL
-        /usr/share/llvm/cmake/                # General shared cmake folder
-        /usr/lib/cmake/llvm/                  # General cmake folder
-      )
-    elseif (WIN32)
-      # Windows platforms using LLVM installer
-      set(search_paths "C:/Program Files/LLVM/lib/cmake/llvm/")
-    else()
-      message(FATAL_ERROR "Unsupported platform")
-    endif()
-  endif()
-
-  find_file(LLVM_CONFIG_CMAKE "LLVM-Config.cmake" PATHS ${search_paths} NO_DEFAULT_PATH)
-  if (NOT LLVM_CONFIG_CMAKE)
-    message(FATAL_ERROR "Could not find LLVM-Config.cmake in the specified paths")
-  endif()
-  get_filename_component(llvm_cmake_dir "${LLVM_CONFIG_CMAKE}" DIRECTORY)
-  set(${llvm_cmake_directory_out_var} "${llvm_cmake_dir}" PARENT_SCOPE)
-endfunction()
-
-function(find_clang_cmake_dir clang_cmake_directory_out_var)
-  if (DEFINED PATH_TO_LLVM)
-    # FIXME(RAKN): this does not work for all Linux locations
-    set(search_paths ${PATH_TO_LLVM}/lib/cmake/clang)
-  else()
-    if (APPLE)
-      if (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
-        # ARM-based Macs (M1/M2) using Homebrew
-        set(search_paths /opt/homebrew/opt/llvm/lib/cmake/clang/)
-      else()
-        # Intel-based Macs using Homebrew
-        set(search_paths /usr/local/opt/llvm/lib/cmake/clang/)
-      endif()
-    elseif (UNIX)
-      # Linux platforms (check common APT/YUM paths for Clang)
-      # FIXME(RAKN): generalize to match llvm-18 til future versions
-      set(search_paths
-        /usr/lib/llvm-10/lib/cmake/clang/    # Example for Clang 10 on Ubuntu/Debian
-        /usr/lib/llvm-12/lib/cmake/clang/    # Example for Clang 12 on Fedora/RHEL
-        /usr/share/clang/cmake/              # General shared CMake folder for Clang
-        /usr/lib/cmake/clang/                # General cmake folder for Clang
-      )
-    elseif (WIN32)
-      # Windows platforms using LLVM/Clang installer
-      set(search_paths "C:/Program Files/LLVM/lib/cmake/clang/")
-    else()
-      message(FATAL_ERROR "Unsupported platform")
-    endif()
-  endif()
-
-  find_file(CLANG_CONFIG_CMAKE "ClangConfig.cmake" PATHS ${search_paths} NO_DEFAULT_PATH)
-  if (NOT CLANG_CONFIG_CMAKE)
-    message(FATAL_ERROR "Could not find ClangConfig.cmake in the specified paths")
-  endif()
-  get_filename_component(clang_cmake_dir "${CLANG_CONFIG_CMAKE}" DIRECTORY)
-  message(STATUS "Found ClangConfig.cmake in: ${clang_cmake_dir}")
-  set(${clang_cmake_directory_out_var} "${clang_cmake_dir}" PARENT_SCOPE)
-endfunction()
-
-# FIXME(RAKN): delete
-function(setup_llvm_cmake_search_paths search_paths_out_var)
-  # TODO(RAKN): support other platforms than MacOS X on M*
-  set(path_to_llvm "/opt/homebrew/opt/llvm/lib/cmake/llvm/")
-  set(${search_paths_out_var}
-    ${path_to_llvm}
-    ${path_to_llvm}/lib/cmake
-    ${path_to_llvm}/lib/cmake/llvm
-    ${path_to_llvm}/lib/cmake/clang
-    ${path_to_llvm}/share/clang/cmake/
-    ${path_to_llvm}/share/llvm/cmake/
-    PARENT_SCOPE
+function(find_brew_prefix brew_prefix_out_var)
+  execute_process(
+    COMMAND brew --version
+    OUTPUT_QUIET
+    ERROR_QUIET
+    RESULT_VARIABLE brew_check_result
   )
+
+  set(${brew_prefix_out_var} "" PARENT_SCOPE)
+  if (brew_check_result EQUAL 0)
+    execute_process(
+      COMMAND brew --prefix
+      OUTPUT_VARIABLE BREW_PREFIX
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(${brew_prefix_out_var} "${BREW_PREFIX}" PARENT_SCOPE)
+  endif()
+endfunction()
+
+# FIXME(RAKN): generate concise usage messages for all optional variables and extend the variables by a prefix
+# decide whether to use CMake options or not
+# FIXME(RAKN): test the whole stuff on Linux and Windows
+# FIXME(RAKN): if the directories were not found explain how to install LLVM in supported ways per platform
+function(find_llvm_cmake_directories SUPPORTED_LLVM_VERSION llvm_cmake_dir_out_var clang_cmake_directory_out_var)
+  function(set_llvm_target_search_paths target search_paths_out_var)
+    if (DEFINED PATH_TO_LLVM)
+      set(search_paths "${PATH_TO_LLVM}/lib/cmake/${target}")
+    elseif (WIN32)
+      set(search_paths
+        "C:/Program Files/LLVM/lib/cmake/${target}/"
+        "C:/Program Files (x86)/LLVM/lib/cmake/${target}/"
+      )
+    else()
+      set(search_paths
+        "/usr/lib/llvm-${SUPPORTED_LLVM_VERSION}/lib/cmake/${target}/"
+        "/usr/share/llvm-${SUPPORTED_LLVM_VERSION}/cmake/${target}/"
+        "/usr/lib/cmake/${target}/"
+      )
+
+      find_brew_prefix(HOMEBREW_PREFIX)
+      if (NOT HOMEBREW_PREFIX STREQUAL "")
+        list(APPEND search_paths "${HOMEBREW_PREFIX}/opt/llvm/lib/cmake/${target}/")
+      endif()
+    endif()
+
+    set(${search_paths_out_var} ${search_paths} PARENT_SCOPE)
+  endfunction()
+
+  set_llvm_target_search_paths("llvm" llvm_search_paths)
+  find_path(LLVM_CMAKE_DIR LLVMConfig.cmake PATHS ${llvm_search_paths} NO_DEFAULT_PATH)
+  if (NOT LLVM_CMAKE_DIR)
+    message(FATAL_ERROR "Could not find LLVM CMake directory in the search paths ${llvm_search_paths}. Please ensure LLVM is installed, and provide its location manually with PATH_TO_LLVM if necessary.")
+  endif()
+
+  set_llvm_target_search_paths("clang" clang_search_paths)
+  find_path(CLANG_CMAKE_DIR ClangConfig.cmake PATHS ${clang_search_paths} NO_DEFAULT_PATH)
+  if (NOT CLANG_CONFIG_CMAKE)
+    message(FATAL_ERROR "Could not find Clang CMake directory in the search paths ${clang_search_paths}. Please ensure LLVM is installed, and provide its location manually with PATH_TO_LLVM if necessary.")
+  endif()
+
+  message(STATUS "Found LLVM CMake directory: ${LLVM_CMAKE_DIR}")
+  set(${llvm_cmake_dir_out_var} "${LLVM_CMAKE_DIR}" PARENT_SCOPE)
+
+  message(STATUS "Found Clang CMake directory: ${CLANG_CMAKE_DIR}")
+  set(${clang_cmake_directory_out_var} "${CLANG_CMAKE_DIR}" PARENT_SCOPE)
+endfunction()
+
+function(check_homebrew_package_installed package_name result_out_var)
+  # Check if brew is installed
+  execute_process(
+    COMMAND brew --version
+    OUTPUT_QUIET
+    ERROR_QUIET
+    RESULT_VARIABLE brew_check_result
+  )
+
+  if (NOT brew_check_result EQUAL 0)
+    set(${result_out_var} FALSE PARENT_SCOPE)
+    return()
+  endif()
+
+  # Check if the package is installed
+  execute_process(
+    COMMAND brew list --formula ${package_name}
+    OUTPUT_QUIET
+    ERROR_QUIET
+    RESULT_VARIABLE package_check_result
+  )
+
+  set(${result_out_var} $<BOOL:${package_check_result} EQUAL 0> PARENT_SCOPE)
+endfunction()
+
+function(find_homebrew_prefix package_name brew_prefix_out_var)
+  execute_process(
+    COMMAND brew --prefix ${package_name}
+    OUTPUT_VARIABLE BREW_PREFIX
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET  # errors are handled below
+  )
+
+  if (NOT BREW_PREFIX)
+    message(FATAL_ERROR "Could not find ${package_name} using Homebrew. Please ensure ${package_name} is installed via `brew install ${package_name}`.")
+  endif()
+
+  message(STATUS "Homebrew prefix for ${package_name}: ${BREW_PREFIX}")
+  set(${brew_prefix_out_var} "${BREW_PREFIX}" PARENT_SCOPE)
+endfunction()
+
+function(find_lit_program lit_path_out_var)
+  if (DEFINED LIT_PATH)
+    message(STATUS "Using user-provided path to lit: ${LIT_PATH}")
+    set(lit_executable ${LIT_PATH})
+  else()
+    check_homebrew_package_installed(lit HOMEBREW_PACKAGE_LIT_INSTALLED)
+    if (HOMEBREW_PACKAGE_LIT_INSTALLED)
+      find_homebrew_prefix(lit LIT_PREFIX)
+      set(lit_executable "${LIT_PREFIX}/bin/lit")
+    elseif (UNIX OR APPLE)  # Unix* platforms using pip to install lit
+      find_program(lit_executable lit HINTS $ENV{HOME}/.local/bin /usr/local/bin /usr/bin)
+    elseif (WIN32)  # Windows platforms using pip to install lit
+      find_program(lit_executable lit HINTS $ENV{APPDATA}/Python/Scripts $ENV{LOCALAPPDATA}/Programs/Python/Scripts)
+    else()
+      message(FATAL_ERROR "Unsupported platform")
+    endif()
+  endif()
+
+  if (NOT lit_executable)
+    message(FATAL_ERROR "Could not find lit program. Please install lit using `brew` or `pip`, or provide the LIT_PATH manually.")
+  endif()
+
+  message(STATUS "Using lit program: ${lit_executable}")
+  set(${lit_path_out_var} "${lit_executable}" PARENT_SCOPE)
 endfunction()
 
 function(setup_precompiled_llvm compiler_location_out_var)
