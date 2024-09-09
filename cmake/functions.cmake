65edc80f40c360dbbab40a0543c417cbc3720b28
Raphael Knaus <rknuus@gmail.com>
2024-09-09 23:06:08 +0200

    WIP Tidy up

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -106,36 +106,25 @@ function(configure_gogoote_version
   GOGOOTE_LINK_FLAGS
   GOGOOTE_CXX_FLAGS
 )
-  # Get the latest git commit hash
-  execute_process(
-    COMMAND git log -1 --format=%h
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE GIT_COMMIT
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+  execute_process(COMMAND git log -1 --format=%h OUTPUT_VARIABLE GIT_COMMIT OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-  # TODO: Support Windows (the `date` command might be different)
-  execute_process(
-    COMMAND date "+%d %b %Y"
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE BUILD_DATE
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+  if (WIN32)
+    execute_process(COMMAND powershell -Command "Get-Date -Format 'dd MMM yyyy'"
+                    OUTPUT_VARIABLE BUILD_DATE
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  elseif (UNIX OR APPLE)
+    execute_process(COMMAND date "+%d %b %Y" OUTPUT_VARIABLE BUILD_DATE OUTPUT_STRIP_TRAILING_WHITESPACE)
+  else()
+    message(FATAL_ERROR "Your platform is not supported. You need a Windows, Unix/Linux or MacOS system.")
+  endif()
 
-  configure_file (
-    ${CMAKE_SOURCE_DIR}/src/Version.cpp
-    ${CMAKE_BINARY_DIR}/src/Version.cpp
-    @ONLY
-  )
+  configure_file(${CMAKE_SOURCE_DIR}/src/Version.cpp ${CMAKE_BINARY_DIR}/src/Version.cpp @ONLY)
 
   add_library(GogooteVersion ${CMAKE_BINARY_DIR}/src/Version.cpp)
 
-  target_include_directories(GogooteVersion PRIVATE
-    ${GOGOOTE_INCLUDE_DIRS}
-  )
-  target_include_directories(GogooteVersion SYSTEM PRIVATE
-    ${THIRD_PARTY_INCLUDE_DIRS}
-  )
+  target_include_directories(GogooteVersion PRIVATE ${GOGOOTE_INCLUDE_DIRS})
+  target_include_directories(GogooteVersion SYSTEM PRIVATE ${THIRD_PARTY_INCLUDE_DIRS})
+
   set_target_properties(GogooteVersion PROPERTIES
     LINK_FLAGS ${GOGOOTE_LINK_FLAGS}
     COMPILE_FLAGS ${GOGOOTE_CXX_FLAGS}
