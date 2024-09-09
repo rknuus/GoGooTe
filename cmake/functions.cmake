cd6c116a1efc8179b1c7f914090afe48bbd6343a
Raphael Knaus <rknuus@gmail.com>
2024-09-09 23:04:07 +0200

    WIP Tidy up

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -100,6 +100,48 @@ function(setup_precompiled_llvm compiler_location_out_var)
   set(${compiler_location_out_var} ${clang_location} PARENT_SCOPE)
 endfunction()
 
+function(configure_gogoote_version
+  GOGOOTE_INCLUDE_DIRS
+  THIRD_PARTY_INCLUDE_DIRS
+  GOGOOTE_LINK_FLAGS
+  GOGOOTE_CXX_FLAGS
+)
+  # Get the latest git commit hash
+  execute_process(
+    COMMAND git log -1 --format=%h
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_COMMIT
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  # TODO: Support Windows (the `date` command might be different)
+  execute_process(
+    COMMAND date "+%d %b %Y"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE BUILD_DATE
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  configure_file (
+    ${CMAKE_SOURCE_DIR}/src/Version.cpp
+    ${CMAKE_BINARY_DIR}/src/Version.cpp
+    @ONLY
+  )
+
+  add_library(GogooteVersion ${CMAKE_BINARY_DIR}/src/Version.cpp)
+
+  target_include_directories(GogooteVersion PRIVATE
+    ${GOGOOTE_INCLUDE_DIRS}
+  )
+  target_include_directories(GogooteVersion SYSTEM PRIVATE
+    ${THIRD_PARTY_INCLUDE_DIRS}
+  )
+  set_target_properties(GogooteVersion PROPERTIES
+    LINK_FLAGS ${GOGOOTE_LINK_FLAGS}
+    COMPILE_FLAGS ${GOGOOTE_CXX_FLAGS}
+  )
+endfunction()
+
 function(add_gogoote_executable)
   set (prefix local)
   set (optionArguments )
